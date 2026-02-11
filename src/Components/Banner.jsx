707da47ccfc4d7b1c1ac6cc5580b346e36849e70
Tasnim813 <tasnim3j@gmail.com>
2026-02-11 11:45:37 +0600

    story card add

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,9 +1,23 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const Banner = () => {
+    const router=useRouter();
+    const handleBtn=()=>{
+    const password=prompt('Enter your password')
+    if(password=='1234'){
+       
+        router.push('/dashboard')
+
+    }
+
+    }
     return (
-        <div className=''>
+        <div className='space-y-5'>
             <h1 className='text-4xl font-bold'>Welcome to our Dev-Story</h1>
+            <button onClick={handleBtn} className="px-4 py-2 rounded bg-sky-500 text-white text-2xl cursor-pointer hover:bg-black duration-200 ">Share-story</button>
         </div>
     );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,12 @@
+
 import Banner from "@/Components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" text-center space-y-5 bg-linear-60 to-sky-100 from-sky-300 p-10 rounded">
+    <div className=" text-center  bg-linear-60 to-sky-100 from-sky-300 p-10 rounded">
+      
       <Banner></Banner>
-      <button className="px-4 py-2 rounded bg-sky-500 text-white text-2xl cursor-pointer hover:bg-black duration-200 ">Share-story</button>
     </div>
   );
 }
